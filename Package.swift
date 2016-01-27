b91b0f4c86546704c85cb4809885060bd281d9b7
Robert F. Dickerson <rfdickerson@gmail.com>
2016-01-27 17:10:22 -0600

    Adding a C modulemap package to libssl and libcrypto

--- Package.swift
+++ Package.swift
@@ -11,5 +11,8 @@ import PackageDescription
 let package = Package(
 	name: "ETSocket",
 	targets: [Target(name: "ETSocket")],
+    dependencies: [
+        .Package(url: "https://github.com/rfdickerson/CCrypto", majorVersion: 1),
+    ],
 	exclude: ["ETSocket.xcodeproj", "ETSocket.xcworkspace", "README.md", "Sources/Info.plist"]
 )
